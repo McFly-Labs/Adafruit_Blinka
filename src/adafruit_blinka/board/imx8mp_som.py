aa76b2b295a95ec5942d1356f4bd1ce44dc46fea
Steamfoundry <222248+steamfoundry@users.noreply.github.com>
2023-03-21 09:40:04 -0600

    Adding new NXP NXP i.MX8MP CPU Adding the NXP NavQPlus, using an i.MX8MP System on Module (SoM)

--- src/adafruit_blinka/board/imx8mp_som.py
+++ src/adafruit_blinka/board/imx8mp_som.py
@@ -1,27 +1,51 @@
 # SPDX-FileCopyrightText: 2021 Melissa LeBlanc-Williams for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
+# All pins are based on the default pinout used for the NavQPlus.  Ports may be enabled or disabled depending on the I/O Mux configured
 """Pin definitions for the Coral Dev Board."""
 
 from adafruit_blinka.microcontroller.nxp.imx8mp import pin
-
 # Board name = RPI name [= alias] = pin name
-I2C2_SDA = D2 = SDA = pin.I2C2_SDA
-I2C2_SCL = D3 = SCL = pin.I2C2_SCL
 
-PWM1 = D12 = pin.PWM1
-PWM2 = D13 = pin.PWM2
-PWM3 = D22 = pin.PWM3
+# For up to documentation on the NXP NavQPlus usage, please see https://nxp.gitbook.io/8mpnavq/dev-guide/hardware-interfaces/i2c
+# I2C1 - Do not use, is configured for the internal PMIC 
+# TODO - implement UART/SPI ports
 
-GPIO_P13 = D27 = pin.GPIO6
-GPIO_P16 = D23 = pin.GPIO73
-GPIO_P18 = D24 = pin.GPIO138
-GPIO_P29 = D5 = pin.GPIO7
-GPIO_P31 = D6 = pin.GPIO8
-GPIO_P36 = D16 = pin.GPIO141
-GPIO_P37 = D26 = pin.GPIO77
+# I2C0 - Do not use, is configured for the internal PMIC 
 
-ECSPI1_MISO = D9 = MISO = pin.ECSPI1_MISO
-ECSPI1_MOSI = D10 = MOSI = pin.ECSPI1_MOSI
-ECSPI1_SCLK = D11 = SCLK = SCK = pin.ECSPI1_SCLK
-ECSPI1_SS0 = D8 = SS0 = pin.ECSPI1_SS0
+# I2C1 - Do not use, is configured for the internal PMIC 
+
+# I2C 2 - CSI 1
+I2C2_SCL = pin.I2C2_SCL 
+I2C2_SDA = pin.I2C2_SDA  
+
+# I2C 3 - CSI 2
+I2C3_SCL = pin.I2C3_SCL 
+I2C3_SDA = pin.I2C3_SDA  
+
+# I2C 4 - RTC, SE050 Secure element 
+I2C4_SCL = pin.I2C4_SCL 
+I2C4_SDA = pin.I2C4_SDA  
+
+
+# I2C 6 - Aux (MUXd with UART4) - Picked for SDA/SCL because it is accessible externally
+
+I2C6_SCL = SCL = pin.I2C6_SCL 
+I2C6_SDA = SDA = pin.I2C6_SDA  
+
+# Aux connector
+GPT1CAPTURE1 = D0 = SAI3_TXC = pin.GPIO5_IO0 # GPIO5.IO[0] 
+GPT1CAPTURE2 = D1 = SAI3_TXD = pin.GPIO5_IO1 # GPIO5.IO[1] 
+
+i2cPorts = (
+    (2, I2C2_SCL, I2C2_SDA),
+    (3, I2C3_SCL, I2C3_SDA),
+    (4, I2C4_SCL, I2C4_SDA),
+    (6, I2C6_SCL, I2C6_SDA),
+)
+
+#
+# UART0 - Bluetooth
+# UART1 on /dev/ttymxc0
+# UART2 - Defaulted to Linux Console for debug
+# UART3 Not available (pins are multiplexed with I2C6)
---
+++ src/adafruit_blinka/microcontroller/nxp/imx8mp/pin.py
@@ -0,0 +1,44 @@
+# SPDX-FileCopyrightText: 2021 Melissa LeBlanc-Williams for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+# All pins are based on the default pinout used for the NavQPlus.  Ports may be enabled or disabled depending on the I/O Mux configured
+# For the pin mapping, check out the CPU datasheet (Preview here -https://www.nxp.com/docs/en/preview/PREVIEW_IMX8MPIEC.pdf ,
+# full version requires NXP account creation)
+"""NXP IMX8MP SOM pin names"""
+from adafruit_blinka.microcontroller.generic_linux.libgpiod_pin import Pin
+
+# TODO - implement UART/SPI ports
+
+# I2C0 - Do not use, connected to the internal PMIC 
+
+# I2C1 - Do not use, connected to the internal PMIC 
+
+# I2C 2 - CSI 1
+I2C2_SCL = Pin((5, 16))  # GPIO5_IO16
+I2C2_SDA = Pin((5, 17))  # GPIO5_IO17
+
+# I2C 3 - CSI 2
+I2C3_SCL = Pin((5, 18))   # GPIO5_IO18
+I2C3_SDA = Pin((5, 19))   # GPIO5_IO19
+
+# I2C 4 - RTC
+I2C4_SCL = Pin((5, 20))   # GPIO5_IO20
+I2C4_SDA = Pin((5, 21))   # GPIO5_IO21
+
+
+# I2C 6 - Aux (MUXd with UART4)
+I2C6_SCL = Pin((5, 28))   # GPIO5_IO28
+I2C6_SDA = Pin((5, 29))   # GPIO5_IO29
+#
+
+i2cPorts = (
+    (2, I2C2_SCL, I2C2_SDA),
+    (3, I2C3_SCL, I2C3_SDA),
+    (4, I2C4_SCL, I2C4_SDA),
+    (6, I2C6_SCL, I2C6_SDA),
+)
+
+#UART and SPI coming still to be implemented
+
+GPIO5_IO0 = Pin((5, 0)) # GPIO5.IO[0] 
+GPIO5_IO1 = Pin((5, 1)) # gpio5.IO[1] 
