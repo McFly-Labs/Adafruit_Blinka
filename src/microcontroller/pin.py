26a5a0afd52d8642e5f9f2dd163ed69604784796
Steamfoundry <222248+steamfoundry@users.noreply.github.com>
2023-05-15 14:29:33 -0600

    fix typo in microcontroller/pin.py for IMX8MP

--- src/microcontroller/pin.py
+++ src/microcontroller/pin.py
@@ -71,7 +71,7 @@ elif chip_id == ap_chip.APQ8016:
     from adafruit_blinka.microcontroller.snapdragon.apq8016.pin import *
 elif chip_id == ap_chip.IMX8MX:
     from adafruit_blinka.microcontroller.nxp_imx8m.pin import *
-elif chip_id == ap_chip.IMX8MX:
+elif chip_id == ap_chip.IMX8MP:
     from adafruit_blinka.microcontroller.nxp.imx8mp.pin import *
 elif chip_id == ap_chip.IMX6ULL:
     from adafruit_blinka.microcontroller.nxp_imx6ull.pin import *
